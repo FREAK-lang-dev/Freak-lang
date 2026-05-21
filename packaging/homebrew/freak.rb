da0960d35f2be887356dcc3c21dce18f854014e2
Otto_606 <147689545+OttoApocalypse69@users.noreply.github.com>
2026-05-22 00:25:03 +0300

    Bump release to 0.14.0

--- packaging/homebrew/freak.rb
+++ packaging/homebrew/freak.rb
@@ -1,7 +1,7 @@
 class Freak < Formula
   desc "FREAK programming language compiler and package manager"
   homepage "https://github.com/FREAK-lang-dev/Freak-lang"
-  version "0.13.3"
+  version "0.14.0"
   license "MIT"
 
   on_macos do
